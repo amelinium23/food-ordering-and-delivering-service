48a921fe503c6432131c89f3232be87960803c6d
Kacper Wiśniewski <230037@edu.p.lodz.pl>
2021-03-26 19:23:46 +0100

    Connect docker to front-end

--- client/App.tsx
+++ client/App.tsx
@@ -1,17 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import * as React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export default function App() : JSX.Element {
+export default function App(): JSX.Element {
   return (
     <View style={styles.container}>
-      <Image source={{
-        uri: "https://i.wpimg.pl/730x0/m.fotoblogia.pl/jajko-688cda13e203b23850b3998237.jpg"
-      }} style={styles.logo}/>
+      <Image
+        source={{
+          uri:
+            "https://i.wpimg.pl/730x0/m.fotoblogia.pl/jajko-688cda13e203b23850b3998237.jpg",
+        }}
+        style={styles.logo}
+      />
       <Text style={styles.instructions}>Czemu ja to robię</Text>
-      <TouchableOpacity onPress={() => alert('Hello world')} style={{backgroundColor: 'blue'}}>
-        <Text style={{color: 'red'}}>Front end design is my passion</Text>
-      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,9 +21,9 @@ export default function App() : JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   logo: {
     width: 305,
@@ -30,8 +31,8 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   instructions: {
-    color: '#888',
+    color: "#888",
     fontSize: 18,
     marginHorizontal: 15,
-  }
+  },
 });
