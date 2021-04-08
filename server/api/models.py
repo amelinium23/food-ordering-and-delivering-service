38f00274b73cab549600b32e3023db708e1aef5c
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-04-09 00:11:18 +0200

    changes to models.py

--- server/api/models.py
+++ server/api/models.py
@@ -4,17 +4,24 @@ from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Restaurant(models.Model):
-    CUISINE_TYPE_CHOICES = ['inne', 'chińska', 'włoska', 'turecka', 'polska', 'indyjska']
+    class CUISINE_TYPE_CHOICES(models.TextChoices):
+        OTHER = 'def', _('inne')
+        CHINESE = 'ch', _('chińska')
+        ITALIAN = 'it', _('włoska')
+        TURKISH = 'tu', _('turecka')
+        POLISH = 'po', _('polska')
+        INDIAN = 'in', _('indyjska')
+        JEWISH = 'je', _('żydowska')
     name = models.CharField(max_length=50)
     logo = models.URLField()
-    address = models.CharField()
-    cuisine_type = models.CharField(choices=CUISINE_TYPE_CHOICES)
+    address = models.CharField(max_length=80)
+    cuisine_type = models.CharField(max_length=5, choices=CUISINE_TYPE_CHOICES.choices)
     delivery_cost = models.DecimalField(max_digits=4, decimal_places=2)
     is_active = models.BooleanField(default=True)
     description = models.TextField(blank=True)
 
 class OpeningHours(models.Model):
-    class Weekday(models.TextChoices):
+    class WEEKDAY(models.TextChoices):
         MONDAY = 1, _('Poniedziałek')
         TUESDAY = 2, _('Wtorek')
         WEDNESDAY = 3, _('Środa')
@@ -22,7 +29,7 @@ class OpeningHours(models.Model):
         FRIDAY = 5, _('Piątek')
         SATURDAY = 6, _('Sobota')
         SUNDAY = 7, _('Niedziela')
-    weekday = models.IntegerField(choices=Weekday.choices)
+    weekday = models.IntegerField(choices=WEEKDAY.choices)
     restaurant_id = models.ForeignKey('Restaurant', on_delete=models.CASCADE)
     openingHour = models.CharField(max_length=5)
     closingHour = models.CharField(max_length=5)
@@ -37,9 +44,12 @@ class Dish(models.Model):
     description = models.TextField(blank=True)
 
 class ExtraGroup(models.Model):
-    type_choices = ['list','bool']
+    class TYPE_CHOICES(models.IntegerChoices):
+        LIST = 1
+        BOOL = 2
     name = models.CharField(max_length=30)
-    extra_type = models.CharField(choices=type_choices)
+    extra_type = models.IntegerField(choices=TYPE_CHOICES.choices)
+
 
 class Extra(models.Model):
     category_id = models.ForeignKey('Dish', on_delete=models.CASCADE)
