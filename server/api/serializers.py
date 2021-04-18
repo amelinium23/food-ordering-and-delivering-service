1404cb032e81295c439f96d92497c8bf04af0b38
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-04-18 23:33:35 +0200

    serializers added

--- server/api/serializers.py
+++ server/api/serializers.py
@@ -1,7 +1,36 @@
 from rest_framework import serializers
-from api.models import Restaurant
+import api.models
 
 class RestaurantSerializer(serializers.ModelSerializer):
    class Meta:
       model = Restaurant
       exclude = ['is_active']
+
+class OpeningHoursSerializer(serializers.ModelSerializer):
+   weekday = serializers.CharField(source='get_weekday_display')
+   class Meta:
+      model = OpeningHours
+      fields = ['weekday', 'openingHour', 'closingHour']
+
+class MenuGroupSerializer(serializers.ModelSerializer):
+   data = DishSerializer(many=True, readonly=True)
+   class Meta:
+      model = MenuGroup
+      fields = ['name', 'data']
+
+class DishSerializer(serializers.ModelSerializer):
+   extras_group = ExtraGroupSerializer(many=True, readonly=True)
+   class Meta:
+      model = Dish
+      fields = ['name', 'price', 'description', 'extras_group']
+
+class ExtraGroupSerializer(serializers.ModelSerializer):
+   extras = ExtraSerializer(many=True, readonly=True)
+   class Meta:
+      model = ExtraGroup
+      fields = ['name', 'extra_type', 'extras']
+
+class ExtraSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = Extra
+      fields = ['name', 'added_price', 'description']
