f8abf02ded803330f4ec1ac43c12af7c545d71a3
kwisnia <230037@edu.p.lodz.pl>
2021-05-29 22:38:31 +0200

    Add __str__ to models

--- server/api/admin.py
+++ server/api/admin.py
@@ -5,19 +5,29 @@ import nested_admin
 
 # Register your models here.
 
+
 class OpeningHourInline(nested_admin.NestedTabularInline):
     model = OpeningHour
+
+
 class ExtraInline(nested_admin.NestedTabularInline):
     model = Extra
+
+
 class ExtraGroupInline(nested_admin.NestedTabularInline):
     model = ExtraGroup
     inlines = [ExtraInline]
+
+
 class DishInline(nested_admin.NestedTabularInline):
     model = Dish
     inlines = [ExtraGroupInline]
+
+
 class MenuGroupInline(nested_admin.NestedTabularInline):
     model = MenuGroup
     inlines = [DishInline]
+
 
 @admin.register(Restaurant)
 class RestaurantAdmin(OSMGeoAdmin, nested_admin.NestedModelAdmin):
@@ -27,3 +37,14 @@ class RestaurantAdmin(OSMGeoAdmin, nested_admin.NestedModelAdmin):
     ]
     list_display = ('name', 'location')
 
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('id', '__str__')
+    readonly_fields = ('id',)
+
+
+@admin.register(Extra)
+class ExtraAdmin(admin.ModelAdmin):
+    list_display = ('id', '__str__')
+    readonly_fields = ('id', )
--- server/api/urls.py
+++ server/api/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('restaurants/', views.RestaurantList.as_view()),
     path('restaurants/<int:pk>/', views.RestaurantDetails.as_view()),
     path('restaurants/<int:pk>/menu', views.RestaurantMenu.as_view()),
-    path('orders/<int:user_id>/', views.OrderHistory.as_view()),
+    path('order-history/<int:user_id>/', views.OrderHistory.as_view()),
+    path('orders/', views.OrderDetails.as_view())
 ]
