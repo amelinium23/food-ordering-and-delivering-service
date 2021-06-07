19788736023c9d219722a466280fc909672cce05
Piotr Tomczak <230025@edu.p.lodz.pl>
2021-06-07 14:09:02 +0200

    Userheadre without putting new data to backend

--- client/components/UserHeader.tsx
+++ client/components/UserHeader.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { View, StyleSheet, Text } from "react-native";
-import { Octicons } from "@expo/vector-icons";
+import { View, StyleSheet, Text, Pressable, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import { User } from "../types/User";
+import { AntDesign } from "@expo/vector-icons";
 
 const UserHeader: React.FunctionComponent<User> = ({
   email,
@@ -10,20 +11,71 @@ const UserHeader: React.FunctionComponent<User> = ({
   last_name,
   address,
 }) => {
-  return (
+  // const handleSubmit = () => {
+  //   const data = {};
+  // };
+  const [editable, setEditable] = React.useState(false);
+  return editable ? (
+    <View style={styles.container}>
+      <Text style={styles.formText}>Nazwa użytkownika: </Text>
+      <TextInput
+        style={styles.input}
+        autoCompleteType="username"
+        autoCapitalize="none"
+        textContentType="username"
+        placeholder="Nazwa użytkownika"
+      />
+      <Text style={styles.formText}>Imię</Text>
+      <TextInput
+        style={styles.input}
+        autoCompleteType="name"
+        autoCapitalize="words"
+        textContentType="name"
+        placeholder="Imię"
+      />
+      <Text style={styles.formText}>Nazwisko</Text>
+      <TextInput
+        style={styles.input}
+        autoCompleteType="name"
+        autoCapitalize="words"
+        textContentType="name"
+        placeholder="Nazwisko"
+      />
+      <Text style={styles.formText}>Mail</Text>
+      <TextInput
+        style={styles.input}
+        autoCompleteType="email"
+        autoCapitalize="none"
+        textContentType="emailAddress"
+        placeholder="Nowy mail"
+      />
+      <Text style={styles.formText}>Adres</Text>
+      <TextInput
+        style={styles.input}
+        autoCompleteType="street-address"
+        autoCapitalize="none"
+        textContentType="streetAddressLine1"
+        placeholder="Nowy adres"
+      />
+      <Pressable style={styles.button} onPress={() => setEditable(!editable)}>
+        <Text style={styles.buttonText}>
+          <AntDesign name="save" size={24} color="white" /> Modyfikuj
+        </Text>
+      </Pressable>
+    </View>
+  ) : (
     <View style={styles.container}>
       <View style={styles.infoContainer}>
-        <Octicons
-          name="octoface"
-          size={42}
-          color="black"
-          style={{ alignSelf: "center" }}
-        ></Octicons>
         <Text style={styles.text}>Nazwa użytkownika: {username}</Text>
         <Text style={styles.text}>Imię: {first_name}</Text>
         <Text style={styles.text}>Nazwisko: {last_name}</Text>
         <Text style={styles.text}>Adres: {address}</Text>
         <Text style={styles.text}>Email: {email}</Text>
+        <Pressable style={styles.button} onPress={() => setEditable(!editable)}>
+          <Text style={styles.buttonText}>
+            <Feather name="edit" size={24} color="white" /> Edytuj
+          </Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -39,7 +91,12 @@ const styles = StyleSheet.create({
     fontSize: 16,
     textAlign: "center",
   },
+  formText: {
+    fontSize: 16,
+    textAlign: "left",
+  },
   infoContainer: {
+    backgroundColor: "white",
     flex: 2,
     padding: 10,
     marginHorizontal: 10,
@@ -62,7 +119,7 @@ const styles = StyleSheet.create({
     width: 200,
     borderRadius: 10,
     borderColor: "black",
-    borderWidth: 1.5,
+    borderWidth: 1,
     shadowColor: "#000",
     shadowOffset: {
       width: 0,
@@ -70,6 +127,24 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.5,
     shadowRadius: 6,
+  },
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    marginVertical: 4,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    elevation: 5,
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "center",
   },
 });
 
