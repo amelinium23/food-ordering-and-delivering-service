1a82a95a626210aa823fea7d5cb96027fdc6cb6f
Mateusz Roszkowski <229990@edu.p.lodz.pl>
2021-06-08 15:49:55 +0200

    Connected user screen to api

--- client/screens/UserScreen.tsx
+++ client/screens/UserScreen.tsx
@@ -1,28 +1,47 @@
 import * as React from "react";
-import axios from "axios";
 import UserHeader from "../components/UserHeader";
+import { User as UserType } from "../types/ApiResponseTypes";
+import UserContext from "../contexts/UserContext";
 
 const UserScreen: React.FunctionComponent = () => {
-  const [userData, setUserData] = React.useState({});
-  // const requestData = async () => {
-  //   const data = await axios.get(
-  //     `https://glove-backend.herokuapp.com/users/users/1`
-  //   );
-  //   console.log(data);
-  //   setUserData(data);
-  // };
+  const [userData, setUserData] = React.useState({
+    email: "example@example.com",
+    username: "example",
+    first_name: "John",
+    last_name: "Doe",
+    address: "",
+    account_type: 1,
+  });
+  const [session, setSession] = React.useContext(UserContext);
 
-  // React.useEffect(() => {
-  //   requestData();
-  // }, []);
+  React.useEffect(() => {
+    const requestData = async () => {
+      const res = await fetch(
+        `https://glove-backend.herokuapp.com/users/auth/user/`,
+        {
+          headers: {
+            // "Content-Type": "application/json",
+            Authorization: `Bearer ${session.token.access_token}`,
+          },
+        }
+      );
+      if (res.ok) {
+        const json = (await res.json()) as UserType;
+        setUserData(json);
+      }
+    };
+
+    void requestData();
+  }, [session]);
 
   return (
     <UserHeader
-      first_name="Andrzej"
-      last_name="Nie wiem"
-      username="To się nie uda"
-      email="example@mail.com"
-      address="A nie wiem o co wam chodzi, Łódź"
+      first_name={userData.first_name}
+      last_name={userData.last_name}
+      username={userData.username}
+      email={userData.email}
+      address={userData.address}
+      account_type={userData.account_type}
     />
   );
 };
--- client/types/User.ts
+++ client/types/User.ts
@@ -4,4 +4,5 @@ export interface User {
   first_name: string;
   last_name: string;
   address: string;
+  account_type: number;
 }
